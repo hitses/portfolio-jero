b241532d82b42e87333cb02e34e342eabd7794cf
Miguel García Gil <miguelg17m@gmail.com>
2023-03-13 13:45:12 +0100

    Todo reactive form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { ReCaptchaV3Service } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  constructor(private recaptchaV3Service: ReCaptchaV3Service) {}
 
+  // TODO : Add REACTIVE form validations
+  // public send(form: NgForm): void {
+  //   if (form.invalid) {
+  //     for (const control of Object.keys(form.controls)) {
+  //       form.controls[control].markAsTouched();
+  //     }
+  //     return;
+  //   }
+
+  //   this.recaptchaV3Service.execute('importantAction')
+  //   .subscribe((token: string) => {
+  //     console.debug(`Token [${token}] generated`);
+  //   });
+  // }
 }
