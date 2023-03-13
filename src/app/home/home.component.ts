8c1d5250509fa9243c1ce778d805b31b1772267d
Miguel García Gil <miguelg17m@gmail.com>
2023-03-13 13:46:58 +0100

    Add home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import jobs from '../../assets/jobs.json';
+import studies from '../../assets/studies.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  jobs = jobs.jobs;
+  studies = studies.studies;
 }
