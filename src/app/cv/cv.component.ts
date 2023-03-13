d82dfce11da5a9c3336681eb6091f8b2de975cdf
Miguel García Gil <miguelg17m@gmail.com>
2023-03-13 13:47:11 +0100

    Add cv component

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.scss']
+  styleUrls: ['./cv.component.scss'],
 })
 export class CvComponent {
+  @Input() title!: string;
+  @Input() ps!: string[];
+  @Input() icons!: string[];
 
+  isHidden: boolean = true;
+
+  toggle() {
+    this.isHidden = !this.isHidden;
+  }
 }
