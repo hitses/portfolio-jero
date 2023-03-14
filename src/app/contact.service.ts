50da15ca844fa4bac8ad3555b4e470f44a2d1951
Miguel García Gil <miguelg17m@gmail.com>
2023-03-14 18:15:00 +0100

    Add HTTP contact form

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Observable } from 'rxjs';
+import { Contact } from './interfaces/main';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
+  private baseUrl: string = environment.baseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  contact(body: any): Observable<Contact> {
+    const url = `${this.baseUrl}/mail/contact`;
+    return this.http.post<Contact>(url, body);
+  }
 }
