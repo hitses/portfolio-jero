8c3103682fde7a8429f7f6c2cc770ad76bef1cdc
Miguel García Gil <miguelg17m@gmail.com>
2023-03-14 20:26:06 +0100

    Add translations

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  language: string = 'en';
+
+  constructor(private translate: TranslateService) {
+    this.translate.use(
+      localStorage.getItem('language') ||
+        this.translate.getBrowserLang() ||
+        this.language
+    );
+  }
+
+  changeLanguage(language: string) {
+    this.translate.use(language);
+    localStorage.setItem('language', language);
+  }
+}
