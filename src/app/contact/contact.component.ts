e7571760e600161311df67dd247d2b5f5dbe7c73
Miguel García Gil <miguelg17m@gmail.com>
2023-03-14 18:16:39 +0100

    Add contact form funcionality

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReCaptchaV3Service } from 'ng-recaptcha';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,20 +9,62 @@ import { ReCaptchaV3Service } from 'ng-recaptcha';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  constructor(private recaptchaV3Service: ReCaptchaV3Service) {}
+  mailSended: boolean = false;
+  sending: boolean = false;
+  contact: FormGroup = this.fb.group({
+    name: ['Jero', [Validators.required, Validators.minLength(2)]],
+    email: [
+      'jero@mailmeloinvento.com',
+      [Validators.required, Validators.email],
+    ],
+    subject: ['Subject', [Validators.required, Validators.minLength(2)]],
+    message: [
+      'Mi comentario',
+      [Validators.required, Validators.maxLength(250)],
+    ],
+  });
+
+  constructor(
+    private recaptchaV3Service: ReCaptchaV3Service,
+    private fb: FormBuilder,
+    private contactService: ContactService
+  ) {}
+
+  invalidField(field: string) {
+    return (
+      this.contact.controls[field].errors &&
+      this.contact.controls[field].touched
+    );
+  }
+
+  send() {
+    if (this.contact.invalid) {
+      this.contact.markAllAsTouched();
+      return;
+    }
+
+    const body = this.contact.value;
+    body.language = 'es';
+
+    this.sending = true;
+
+    this.contactService.contact(body).subscribe((res) => {
+      this.recaptchaV3Service
+        .execute('importantAction')
+        .subscribe((token: string) => {
+          console.debug(`Token [${token}] generated`);
+        });
+
+      if (res.status === 200) {
+        this.mailSended = true;
+        this.sending = false;
+      }
+      this.contact.reset();
+    });
+  }
 
   // TODO : Add REACTIVE form validations
   // public send(form: NgForm): void {
-  //   if (form.invalid) {
-  //     for (const control of Object.keys(form.controls)) {
-  //       form.controls[control].markAsTouched();
-  //     }
-  //     return;
-  //   }
 
-  //   this.recaptchaV3Service.execute('importantAction')
-  //   .subscribe((token: string) => {
-  //     console.debug(`Token [${token}] generated`);
-  //   });
   // }
 }
